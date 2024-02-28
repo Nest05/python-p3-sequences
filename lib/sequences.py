16214ae743f68b11249de472951901120a172b66
Nestor Masinde <nestormasinde@gmail.com>
2024-02-28 06:56:24 +0300

    sequences covered!

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print(fibonacci_sequence[:1])
+    elif length == 2:
+        print(fibonacci_sequence)
+    else:
+        while len(fibonacci_sequence) < length:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
+   
